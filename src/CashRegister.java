e29ea3423c958d3abc660d22be1b29d494982614
jbacstarter <lightningjohnftw@gmail.com>
2024-09-27 22:26:27 +0800

    Finished making the CashRegister and DispenserType classes..

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,4 +1,18 @@
 
 public class CashRegister {
-
+	private int cashOnHand = 0;
+	public CashRegister() {
+		this.cashOnHand = 500;
+	};
+	
+	public CashRegister(int cashIn) {
+		this.cashOnHand = cashIn;
+	};
+	public void acceptAmount(int amountIn) {
+		if(amountIn <= 0)System.err.println("ERROR: Invalid amount deposited ");
+		this.cashOnHand+=amountIn;
+	};
+	public int getCurrentBalance() {
+		return cashOnHand;
+	}
 }
--- src/DispenserType.java
+++ src/DispenserType.java
@@ -1,4 +1,32 @@
 
 public class DispenserType {
 
+	public DispenserType() {
+		this.cost = 50;
+		this.numberOfItems = 50;
+	}
+	public DispenserType(int num,int cost) {
+		this.numberOfItems = num;
+		this.cost = cost;
+	}
+	
+	
+	public int getNumberOfItems() {
+		return numberOfItems;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+	public void makeSale() {
+		int temp = this.numberOfItems;
+		temp--;
+		this.numberOfItems = temp;
+	}
+
+
+	private int numberOfItems = 0;
+	private int cost = 0;
+	
+	
 }
