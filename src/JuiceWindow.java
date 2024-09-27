195645b0ea9566a0d4af7cae944aed08bd95f503
jbacstarter <lightningjohnftw@gmail.com>
2024-09-27 23:57:47 +0800

    finished adding the selection panel in JuiceWindow class.

--- src/JuiceWindow.java
+++ src/JuiceWindow.java
@@ -1,24 +1,57 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class JuiceWindow extends JFrame {
 
 	private JuiceMachine jm;
-	private JButton apple;
-	private JButton orange;
-	private JButton mango;
-	private JButton fruit;
+	
+	private JButton 
+	apple = new JButton("APPLE"), 
+	orange = new JButton("ORANGE"),
+	mango = new JButton("MANGO"),
+	fruit = new JButton("FRUIT");
+	
+	private JPanel selectionPanel = new JPanel();
+	
 	public JuiceWindow(JuiceMachine jm) {
 		this.jm = jm;
 		this.initGUI();
+		this.initSelectPanel();
+		this.setVisible(true);
 	}
 	
-	public void initGUI() {
+	private void initGUI() {
 		this.setSize(600, 420);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Fruit Juice Machine");
 		this.getContentPane().setLayout(null);
-		this.setVisible(true);
 	}
+	
+	private void initSelectPanel() {
+		selectionPanel.setLayout(new GridLayout(4,1, 0, 20));
+		//selectionPanel.setBackground(Color.blue);
+		selectionPanel.setBounds(50,35,200, 200);
+		this.add(selectionPanel);
+		
+		apple.setText("Apple");
+		apple.setBackground(Color.pink);
+		orange.setText("Orange");
+		orange.setBackground(Color.orange);
+		mango.setText("Mango");
+		mango.setBackground(Color.yellow);
+		fruit.setText("Fruit");
+		fruit.setBackground(Color.MAGENTA);
+		
+		
+		selectionPanel.add(apple);
+		selectionPanel.add(orange);
+		selectionPanel.add(mango);
+		selectionPanel.add(fruit);
+	}
+
+	
 }
