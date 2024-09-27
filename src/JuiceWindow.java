e1a4697f5dad15549e26f2ac603fa7fb36334d39
jbacstarter <lightningjohnftw@gmail.com>
2024-09-28 01:39:00 +0800

    Added button listeners for the juice buttons

--- src/JuiceWindow.java
+++ src/JuiceWindow.java
@@ -1,7 +1,8 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -12,6 +13,7 @@ public class JuiceWindow extends JFrame {
 		this.initGUI();
 		this.initSelectPanel();
 		this.initDetailsPanel();
+		this.buttonListeners();
 		this.setVisible(true);
 	}
 	
@@ -23,6 +25,7 @@ public class JuiceWindow extends JFrame {
 		this.setTitle("Fruit Juice Machine");
 		//this.getContentPane().setBackground(Color.darkGray);
 		this.getContentPane().setLayout(null);
+		
 	}
 	
 	private void initSelectPanel() {
@@ -46,22 +49,24 @@ public class JuiceWindow extends JFrame {
 		selectionPanel.add(mango);
 		selectionPanel.add(fruit);
 	}
+	
+	
 	// Name of product, Cost, Availability
 	private void initDetailsPanel() {
 		detailsPanel.setLayout(new GridLayout(3,1, 0, 25));
-		detailsPanel.setBackground(Color.lightGray);
-		detailsPanel.setBounds(380,10,200, 150);
+		detailsPanel.setBackground(Color.gray);
+		detailsPanel.setBounds(380,10,205, 150);
 		
 		this.add(detailsPanel);
 		
 		pName.setText("Juice: ");
 		//pName.setBackground(Color.lightGray);
-		pName.setFont(new Font("Times New Roman", Font.BOLD | Font.CENTER_BASELINE, 25));
+		pName.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
 		
 		cost.setText("Cost: ");
-		cost.setFont(new Font("Times New Roman", Font.BOLD | Font.CENTER_BASELINE, 25));
-		avail.setText("Available: ");
-		avail.setFont(new Font("Times New Roman", Font.BOLD | Font.CENTER_BASELINE, 25));
+		cost.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
+		
+		avail.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
 		pName.setEditable(false);
 		cost.setEditable(false);
 		avail.setEditable(false);
@@ -72,7 +77,57 @@ public class JuiceWindow extends JFrame {
 		detailsPanel.add(avail);
 	}
 		
-		
+		private void buttonListeners() {
+			// Apple button press listener
+			apple.addActionListener(new ActionListener() {
+				DispenserType a = jm.getApple();
+				public void actionPerformed(ActionEvent e) {
+					pName.setText("Juice: " + "Apple");
+					cost.setText("Cost: " + a.getCost());
+					boolean res = a.isSoldOut();
+					if(res == true) avail.setForeground(Color.red);
+					else avail.setForeground(Color.green);
+					String str = a.isSoldOut() == true ? "Sold Out!" : "Available";
+					avail.setText(str);
+				}
+			});
+			orange.addActionListener(new ActionListener() {
+				DispenserType a = jm.getOrange();
+				public void actionPerformed(ActionEvent e) {
+					pName.setText("Juice: " + "Orange");
+					cost.setText("Cost: " + a.getCost());
+					boolean res = a.isSoldOut();
+					if(res == true) avail.setForeground(Color.red);
+					else avail.setForeground(Color.green);
+					String str = a.isSoldOut() == true ? "Sold Out!" : "Available";
+					avail.setText(str);
+				}
+			});
+			mango.addActionListener(new ActionListener() {
+				DispenserType a = jm.getMango();
+				public void actionPerformed(ActionEvent e) {
+					pName.setText("Juice: " + "Mango lassi");
+					cost.setText("Cost: " + a.getCost());
+					boolean res = a.isSoldOut();
+					if(res == true) avail.setForeground(Color.red);
+					else avail.setForeground(Color.green);
+					String str = a.isSoldOut() == true ? "Sold Out!" : "Available";
+					avail.setText(str);
+				}
+			});
+			fruit.addActionListener(new ActionListener() {
+				DispenserType a = jm.getFruit();
+				public void actionPerformed(ActionEvent e) {
+					pName.setText("Juice: " + "Fruit punch");
+					cost.setText("Cost: " + a.getCost());
+					boolean res = a.isSoldOut();
+					if(res == true) avail.setForeground(Color.red);
+					else avail.setForeground(Color.green);
+					String str = a.isSoldOut() == true ? "Sold Out!" : "Available";
+					avail.setText(str);
+				}
+			});
+		}
 		
 
 	// ALL VARIABLES BELOW
